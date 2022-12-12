3997d7288646791b5e3f79152ae340adee88bce1
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-12-12 11:52:29 +0100

    Support Spring Boot 3.

--- src/main/java/dk/kvalitetsit/prometheus/app/info/actuator/configuration/ActuatorAppInfoAutoConfiguration.java
+++ src/main/java/dk/kvalitetsit/prometheus/app/info/actuator/configuration/ActuatorAppInfoAutoConfiguration.java
@@ -6,14 +6,14 @@ import dk.kvalitetsit.prometheus.app.info.actuator.VersionInfoContributor;
 import dk.kvalitetsit.prometheus.app.info.actuator.VersionProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import java.util.List;
 
-@Configuration
+@AutoConfiguration
 @PropertySource("actuator.properties")
 @PropertySource(value = "actuator-custom.properties", ignoreResourceNotFound = true)
 @PropertySource(value = "git.properties", ignoreResourceNotFound = true)
